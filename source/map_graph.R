1a7c8a2793a1c8493a99fe295c9531e155cd0439
Ryan <ryanfriesz@gmail.com>
2022-02-17 14:24:28 -0800

    changes to world map

--- source/map_graph.R
+++ source/map_graph.R
@@ -1,32 +1,45 @@
-worlddata <- read_OWID_country_emission()
+rm(list = ls())
+setwd("C:/Users/ryanf/Desktop/Data Science 201/final-project-starter-Coevetive/source")
+source("data_access.R")
+owid_emissions_data <- read_OWID_country_emission()
 
-install.packages(c("cowplot", "googleway", "ggplot2", "ggrepel", "ggspatial", "libwgeom", "sf", "rnaturalearth", "rnaturalearthdata"))
-install.packages(c("tidyverse", "maps", "mapproj", "patchwork"))
+
 library("tidyverse")
+library("dplyr")
 library("maps")
 library("mapproj")
 library("patchwork")
-library("dplyr")
-library("ggplot2")
-theme_set(theme_bw())
-library("sf")
 
-library("rnaturalearth")
-library("rnaturalearthdata")
-
-
-world <- ne_countries(scale = "medium", returnclass = "sf")
-class(world)
-
-ggplot(data = world) +
-  geom_sf()
-
-worlddata <- worlddata[worlddata$year == "2020",]
-worlddata <- worlddata[, -c(5:58)]
 
 country_shape <- map_data("world") %>%
-  unite(polyname, region, subregion, sep = ",")%>%
-  left_join(iso.alpha(country_shape, n=3), by="polyname")
+  rename(country = region)
+  
+owid_emissions_data_2020 <- merge(owid_emissions_data, country_shape, by="country")
+
+owid_emissions_data_2020 <- owid_emissions_data %>%
+  filter(year == "2020")%>% 
+  select(country, co2)
 
 
-View(country_shape)
+blank_theme <- theme_bw() +
+  theme(
+    axis.line = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    axis.title = element_blank(),
+    plot.background = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank()
+  )
+
+co2_emissions_map <- ggplot(owid_emissions_data_2020)+
+  geom_polygon(mapping = aes(group = group, x = long, y = lat, fill = co2), color = "gray", size = 0.3)+
+  coord_map()+
+  scale_fill_continuous(limits = c(0, max(owid_emissions_data_2020$co2)), na.value = "white", low = "yellow", high = "red")+
+  blank_theme
+
+
+print(co2_emissions_map)  
+
+dev.off()
