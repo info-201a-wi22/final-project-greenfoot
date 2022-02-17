01cd7a0159b8172d818cce0d91703a861d0f2456
tomjubin <97139089+tomjubin@users.noreply.github.com>
2022-02-17 12:43:40 -0800

    updated read data by year function

--- source/data_access.R
+++ source/data_access.R
@@ -20,8 +20,8 @@ read_OWID_climate_budget <- function () {
   return(df)
 }
 
-read_table <- function () {
-  filepath <- "../data/EPA_GHGRP/Copy_2_of_ghgp_data_by_year.csv"
+read_GHGP_data_by_year <- function () {
+  filepath <- "../data/EPA_GHGRP/Copy_3_of_ghgp_data_by_year.csv"
   df <- read.csv(filepath)
   return(df)
 }
