a5db7c142594038daf28326b4be77142aea5115b
Justin Rhinehart <jjrhine@uw.edu>
2022-02-18 14:21:07 -0800

    updating euroco2 data with sigvar

--- source/c02_Euro_avg.R
+++ source/c02_Euro_avg.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(leaflet)
 library(plotly)
 
-setwd("D:/Downloads/INFO Workspace/final-project-starter-Coevetive/source")
+# setwd("D:/Downloads/INFO Workspace/final-project-starter-Coevetive/source")
 
 source("data_access.R")
 
@@ -17,6 +17,7 @@ euroC02avgs <- euroEmissions %>%
   mutate(yoyMeans = mean(emissions, na.rm = TRUE)) %>%
   distinct(yoyMeans)
 
+Decrease20192020 <- euroC02avgs$yoyMeans[euroC02avgs$reportingYear == 2019] - euroC02avgs$yoyMeans[euroC02avgs$reportingYear == 2020]
 
 bar_graph <- plot_ly(
   data = euroC02avgs, 
