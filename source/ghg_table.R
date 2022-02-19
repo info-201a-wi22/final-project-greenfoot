cdd18ddcaa567fc63fa8f37b418f9fc7a2e0cc75
tomjubin <97139089+tomjubin@users.noreply.github.com>
2022-02-18 16:47:03 -0800

    edited ghg table

--- source/ghg_table.R
+++ source/ghg_table.R
@@ -18,7 +18,9 @@ table_2020_sum_of_total_reported_direct_emissions <- emissions %>%
   group_by(State) %>%
   summarize(Emissions_2020=sum(as.numeric(gsub(",", "",X2020.Total.reported.direct.emissions),na.rm=TRUE),na.rm=TRUE)) 
 
+#full table will display total reported direct emissions for all states dating back to 2017
 #full_table <- data.frame(table_2020_sum_of_total_reported_direct_emissions, Emissions_2019=table_2019_sum_of_total_reported_direct_emissions$Emissions_2019, Emissions_2018=table_2018_sum_of_total_reported_direct_emissions$Emissions_2018, Emissions_2017=table_2017_sum_of_total_reported_direct_emissions$Emissions_2017)
+
 prop_table <- data.frame(table_2020_sum_of_total_reported_direct_emissions, Emissions_2019=table_2019_sum_of_total_reported_direct_emissions$Emissions_2019)
 table <- prop_table
 table$Emissions_Difference <- table_2020_sum_of_total_reported_direct_emissions$Emissions_2020-table_2019_sum_of_total_reported_direct_emissions$Emissions_2019
